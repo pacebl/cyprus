f5255fffed59513648e5a03a97a8d8d1fbde831d
Blaine Pace <blainepace@gmail.com>
2011-01-28 14:41:58 -0800

    Added config test class

---
+++ cypruslib/configtest.py
@@ -0,0 +1,31 @@
+# This file is part of Cyprus.
+#
+# Cyprus is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Cyprus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cyprus.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+import os
+
+import config
+
+class ConfigTest(unittest.TestCase):
+    def setUp(self):
+        self.conf = config.Config()
+
+    def test_check_config(self):
+        self.conf.check_config()
+        path = self.conf.configfile
+        self.assertTrue(os.path.exists(path))
+
+if __name__ == '__main__':
+    unittest.main()
