3f5f673c8a54ccf9aafe91693514cd77051542ec
Blaine Pace <blainepace@gmail.com>
2011-04-28 01:21:15 -0700

    Cyprus class now checks if any movies are found before printing results.

--- cyprus.rb
+++ cyprus.rb
@@ -13,9 +13,13 @@ class Cyprus
     puts "Looking up #{@movie}"
     movie = @imdb.find_by_title(@movie)
     puts
-    puts "Title: #{movie[0][:title]}"
-    puts "Year: #{movie[0][:year]}"
-    puts "Poster URL #{movie[0][:poster_url]}"
+    if movie.empty?
+      puts "Movie not found."
+    else
+      puts "Title: #{movie[0][:title]}"
+      puts "Year: #{movie[0][:year]}"
+      puts "Poster URL #{movie[0][:poster_url]}"
+    end
   end
 end
 
