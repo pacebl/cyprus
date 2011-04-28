b3b9ec2865280e559fb02905d3b96a6f2c5cdef3
Blaine Pace <blainepace@gmail.com>
2011-04-28 01:07:59 -0700

    Cyprus now takes commandline arguments and searches for results on Imdb

--- cyprus.rb
+++ cyprus.rb
@@ -6,12 +6,22 @@ class Cyprus
   def initialize(movie)
     puts "Welcome to Cyprus"
     @movie = movie
+    @imdb = ImdbParty::Imdb.new
   end
 
   def lookup()
     puts "Looking up #{@movie}"
+    movie = @imdb.find_by_title(@movie)
+    puts
+    puts "Title: #{movie[0][:title]}"
+    puts "Year: #{movie[0][:year]}"
+    puts "Poster URL #{movie[0][:poster_url]}"
   end
 end
 
-cp = Cyprus.new "Inception"
-cp.lookup
+if __FILE__ == $0
+  ARGV.each do |a|
+    cp = Cyprus.new(a)
+    cp.lookup
+  end
+end
